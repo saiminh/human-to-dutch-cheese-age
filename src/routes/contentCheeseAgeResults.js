694be38390e755e2e25a9ea6c41a1b35a2999420
saiminh <saiminh@gmail.com>
2022-12-18 11:10:08 +0100

    content

--- src/routes/contentCheeseAgeResults.js
+++ src/routes/contentCheeseAgeResults.js
@@ -12,7 +12,7 @@ export const cheeseAgeResults = [ {
     original: 'No cheese',
     english: 'not yet at least'
   },
-  description: '<p>Basically at this stage, you are still just a metaphorical sludge of milk, acid and bacteria trying to negotiate your future.</p><p>The better regulated the environment you are in, the higher your chances of becoming flavourful and fragrant. What happens in this phase is hugely important for the future, but at this stage, you really don\'t have anything to give yet.</p><p>Just kick back and let nature do it\'s thing. Grow. Fester. Mold. <strong>Mature</strong>.</p>',
+  description: '<p>Basically at this stage, you are still just a metaphorical sludge of milk, acid and bacteria trying to negotiate your future.</p><p>The better regulated the environment you are in, the higher your chances of becoming flavourful and fragrant. What happens in this phase is hugely important for the future, but at this stage, you really don\'t have anything to give yet.</p><p>Just kick back and let nature do its thing. Grow. Fester. Mould. <strong>Mature</strong>.</p>',
   buttontext: 'Then what am I?',
   image: 'illu-not-yet.png'
 } , {
@@ -21,7 +21,7 @@ export const cheeseAgeResults = [ {
     original: 'Jong',
     english: 'Young'
   },
-  description: '<p>Young, fresh, and full of potential.</p><p>For some people, this is the only cheese they\'ll ever be able to understand. Pure, creamy and innocent, in this phase of life you are easy to enjoy, if maybe a little limited and unoriginal.</p> <p>You melt easily and often. And when you do melt you get a bit rubbery.</p>',
+  description: '<p>Young, fresh, and full of potential.</p><p>For some people, this is the only cheese they\'ll ever be able to understand. Pure, creamy and innocent, you are easy to enjoy in this phase of life, if maybe a little limited and unoriginal.</p> <p>You melt easily and often. And when you do melt you get a bit rubbery.</p>',
   buttontext: 'Tell me more',
   image: 'illu-jong.png'
 } , {
@@ -30,7 +30,7 @@ export const cheeseAgeResults = [ {
     original: 'Jong belegen',
     english: 'Matured young'
   },
-  description: '<p>Jong Belegen is the shortest cheese age of all. By definition an in-between state. </p><p>You are neither here nor there. Not young and innocent anymore, but still too green to have made a major dent anywhere in life.</p><p>Enjoy the time you have left not being taken seriously. Life will get a lot more judgy after this.</p><p>Oh and don\'t be one of those lemon flavoured cheeses, develop your own pizazz!</p>',
+  description: '<p>Jong Belegen by definition is an in-between state. </p><p>You are neither here nor there. Not young and innocent anymore, but still too green to have made a major dent anywhere in life.</p><p>Enjoy the time you have left not being taken seriously. Life will get a lot more judgy after this.</p><p>Oh and don\'t be one of those lemon-flavoured cheeses, develop your own pizzazz!</p>',
   buttontext: 'Tell me more',
   image: 'illu-jong-belegen.png'
 } , {
@@ -39,7 +39,7 @@ export const cheeseAgeResults = [ {
     original: 'Belegen',
     english: 'Matured'
   },
-  description: '<p>Yes, you have reached a really nice and balanced ratio between water, salt and eggwhites.</p><p>Slowly, the finer details of what makes life exciting become clear. </p><p>You look back at your earlier years and realize they were merely a lead-up to this. But you also discover that there\'s still so much more potential. </p><p>You might think that you know some stuff now, and that you have achieved at least one or two things. But you don\'t and you haven\'t.</p><p>Keep going, it\'ll get even better!</p>',
+  description: '<p>Yes, you have reached a really nice and balanced ratio of water, salt and protein.</p><p>Slowly, the finer details of what makes life exciting become clear. </p><p>You look back at your earlier years and realize they were merely a lead-up to this. Maybe you think that you know some stuff now, and that you have achieved at least one or two things. But you don\'t and you haven\'t.</p><p>But you also get a whiff of your immense potential.</p>',
   buttontext: 'Tell me more',
   image: 'illu-belegen.png'
 } , {
@@ -48,7 +48,7 @@ export const cheeseAgeResults = [ {
     original: 'Extra Belegen',
     english: 'Extra Matured'
   },
-  description: '<p>In this phase you realize that your appearance is slowly loosing some of its glow and sheen.</p><p><strong>But</strong> it\'s totally worth it! Because while you keep losing water, <strong>you will gain flavour and stank!</strong></p><p>It\'s an exciting process during which you develop more and more finesse, sass and even some grace.</p><p>Of course in this phase you will also catch a first glimpse of your own mortality on the horizont.</p>',
+  description: '<p>In this phase you realize that your appearance is slowly losing some of its glow and lustre.</p><p><strong>But</strong> it\'s totally worth it! Because while you keep losing water, <strong>you will gain flavour and stank!</strong></p><p>It\'s an exciting process during which you develop more and more finesse, sass and even some grace.</p><p>Of course in this phase you will also catch a first glimpse of your own mortality on the horizon.</p>',
   buttontext: 'Tell me more',
   image: 'illu-extra-belegen.png'
 } , {
@@ -57,7 +57,7 @@ export const cheeseAgeResults = [ {
     original: 'Oud',
     english: 'Old'
   },
-  description: '<p>In the Netherlands, people love to state facts without considering anyone\'s feelings. Calling someone old is not an offence here, it\'s just a reference to the fact you\'ve been around for a while.</p><p>By now, you have earned status and respect.</p><p>People that bring you to parties proudly introduce you by pointing out where you\'re from and which circumstances of your life are responsible for the way you smell.</p>',
+  description: '<p>Dutch people are very direct. Calling someone old is not meant as an offence, it\'s just a reference to the fact you\'ve been around for a while.</p><p>By now, you have earned status and respect.</p><p>People who bring you to parties proudly introduce you by pointing out where you\'re from and which circumstances of your life are responsible for your sensational aroma.</p>',
   buttontext: 'Tell me more',
   image: 'illu-oud.png'
 } , {
@@ -66,7 +66,7 @@ export const cheeseAgeResults = [ {
     original: 'Overjarig',
     english: 'Perennial'
   },
-  description: '<p>As a top-shelf cheese you have hardened quite a bit, oh yes. Getting to the top takes its toll. But!</p><p>Salt crystals are flamboyantly sparkling on your skin and proclaim you are strange, fragrant and full of flavour. And you are ready. For anything.</p><p>Enjoy and celebrate yourself. The next (and ultimate) step is purely optional.</p>',
+  description: '<p>As a top-shelf cheese you have hardened quite a bit, oh yes. Getting to the top takes its toll. But!</p><p>Salt crystals are flamboyantly sparkling on your skin. You are strange, fragrant and full of flavour. And you are ready. For anything.</p><p>Enjoy and celebrate yourself. The next (and ultimate) step is purely optional.</p>',
   buttontext: 'Tell me more',
   image: 'illu-overjarig.png'
 } , {
@@ -75,7 +75,7 @@ export const cheeseAgeResults = [ {
     original: 'Brokkelkaas',
     english: 'Crumble Cheese'
   },
-  description: '<p>You are very dry and very salty now.</p><p>While your body cracks and parts fall off spontaneously, you have reached peak flavour. There\'s very few like you, but the impact you can have with a small contribution is immense.</p><p>There\'s certainly no way anyone is going to try and slice you or wrap you in ordinary plastic and sell you out for cheap now.</p>',
+  description: '<p>You are very dry and very salty now.</p><p>While your body cracks and parts fall off spontaneously, you have reached peak flavour. There are very few like you, but the impact you can have with a small contribution is immense.</p><p>There\'s certainly no way anyone is now going to slice you, wrap you in cheap plastic and peddle you.</p>',
   buttontext: 'Tell me more',
   image: 'illu-brokkelkaas.png'
 } ];
